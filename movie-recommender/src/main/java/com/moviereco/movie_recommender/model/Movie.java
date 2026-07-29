994fe89ea6436858ed546dc108cd698438c3871e
Mannraj Singh <msingh58@students.cs.ubc.ca>
2026-07-29 15:34:05 -0700

    Created Movie class with table mapping.

---
+++ movie-recommender/src/main/java/com/moviereco/movie_recommender/model/Movie.java
@@ -0,0 +1,114 @@
+package com.moviereco.movie_recommender.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Movie List")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String imdbId;
+
+    
+    private String title;
+
+    @Column(name = "release_year")
+    private int year;
+
+    @Column(length = 500)
+    private String genre;
+
+    private String director;
+
+    @Column(length = 1000)
+    private String actors;
+
+    private double imdbRating;
+
+    private String posterUrl;
+
+    public Movie() {
+        // required by JPA
+    }
+
+    public Movie(Long id, String imdbId, String title, int year, String genre, String director, String actors, double imdbRating, String posterUrl) {
+        this.id = id;
+        this.imdbId = imdbId;
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.director = director;
+        this.actors = actors;
+        this.imdbRating = imdbRating;
+        this.posterUrl = posterUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public String getImdbId() {
+        return imdbId;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setImdbId(String imdbId) {
+        this.imdbId = imdbId;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getActors() {
+        return actors;
+    }
+
+    public void setActors(String actors) {
+        this.actors = actors;
+    }
+
+    public double getImdbRating() {
+        return imdbRating;
+    }
+
+    public void setImdbRating(double imdbRating) {
+        this.imdbRating = imdbRating;
+    }
+
+    public String getPosterUrl() {
+        return posterUrl;
+    }
+
+    public void setPosterUrl(String posterUrl) {
+        this.posterUrl = posterUrl;
+    }
+}
