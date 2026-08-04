0f342aa3e2e771c02176c4d5795439d7340f5f8f
Mannraj Singh <msingh58@students.cs.ubc.ca>
2026-08-04 15:39:53 -0700

    Added Movie Controller with CRUD endpoints

---
+++ movie-recommender/src/main/java/com/moviereco/movie_recommender/controller/MovieController.java
@@ -0,0 +1,65 @@
+package com.moviereco.movie_recommender.controller;
+
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.moviereco.movie_recommender.model.Movie;
+import com.moviereco.movie_recommender.service.MovieService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+
+
+
+@RestController
+@RequestMapping("/api/movies")
+public class MovieController {
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping
+    public List<Movie> getAllMovies() {
+        return movieService.getMovies();
+    }
+
+    @GetMapping("/{id}")
+    public Movie getMovie(@PathVariable Long id) {
+        return movieService.getMovieById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Movie createMovie(@RequestBody Movie movie) {
+        return movieService.saveMovie(movie);
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteMovie(@RequestParam Long id) {
+        movieService.deleteMovie(id);
+    }
+    
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<Map<String, String>> handleNoSuchElementException(NoSuchElementException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", e.getMessage()));
+    }
+    
+
+}
